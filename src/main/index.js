16ac56d58c2803753876dc9721097bf7aef57c8f
Dylan Almond <dylan@dylanalmond.net>
2024-02-25 23:44:30 +0000

    Added notifications + LICENSE.md

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,25 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
-import sqlite3 from 'sqlite3'
+import {
+  app,
+  shell,
+  BrowserWindow,
+  ipcMain,
+  Notification,
+  Tray,
+  Menu
+} from 'electron';
+import { join } from 'path';
+import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import icon from '../../resources/icon.png?asset';
+import sqlite3 from 'sqlite3';
+
+let mainWindow;
+let isQuiting;
+let tray;
+let database;
 
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1200,
     height: 670,
     show: false,
@@ -17,23 +30,31 @@ function createWindow() {
       sandbox: false,
       nodeIntegration: true
     }
-  })
+  });
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    return { action: 'deny' };
+  });
+
+  mainWindow.on('close', (e) => {
+    if (!isQuiting) {
+      e.preventDefault();
+      mainWindow.hide();
+      e.returnValue = false;
+    }
+  });
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
 }
 
@@ -42,105 +63,262 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.electron');
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+    optimizer.watchWindowShortcuts(window);
+  });
 
   // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
+  ipcMain.on('ping', () => console.log('pong'));
 
-  createWindow()
+  createWindow();
+
+  tray = new Tray(icon);
+
+  tray.setContextMenu(
+    Menu.buildFromTemplate([
+      {
+        label: 'Open',
+        click: function () {
+          mainWindow.show();
+        }
+      },
+      {
+        label: 'Quit',
+        click: function () {
+          app.isQuiting = true;
+          app.quit();
+        }
+      }
+    ])
+  );
+
+  initDB();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('before-quit', function () {
+  isQuiting = true;
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
+function initDB() {
+  database = new sqlite3.Database('./resources/db.sqlite3', (err) => {
+    if (err) console.error('Database opening error: ', err);
+  });
 
-const database = new sqlite3.Database('./resources/db.sqlite3', (err) => {
-  if (err) console.error('Database opening error: ', err)
-})
+  database.run(
+    `
+    CREATE TABLE IF NOT EXISTS todos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT,
+      description TEXT,
+      dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
+      dateDue DATETIME,
+      notificationSentToday BOOLEAN DEFAULT FALSE,
+      notificationSentTomorrow BOOLEAN DEFAULT FALSE
+    )
+  `,
+    (err) => {
+      if (err) console.error('Table creation error: ', err);
+      else {
+        const notification = new Notification({
+          title: 'Connected to DB',
+          body: `Connected to database successfully.`
+        });
 
-database.run(
-  `
-  CREATE TABLE IF NOT EXISTS todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    description TEXT,
-    dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
-    dateDue DATETIME
-  )
-`,
-  (err) => {
-    if (err) console.error('Table creation error: ', err)
-    else console.log('Connected to db:', database)
-  }
-)
+        notification.show();
+
+        checkDueDates();
+
+        setInterval(checkDueDates, 30 * 60 * 1000); // Run every 30 mins
+      }
+    }
+  );
+}
 
 // CRUD operations
 
-// Create operation
+// Create
 ipcMain.on('create-todo', (_, todo) => {
-  const { title, description, dateDue } = todo
+  const { title, description, dateDue } = todo;
 
   if (!title || !dateDue) {
-    return
+    return;
   }
 
-  const query = 'INSERT INTO todos (title, description, dateDue) VALUES (?, ?, ?)';
+  const query =
+    'INSERT INTO todos (title, description, dateDue) VALUES (?, ?, ?)';
   database.run(query, [title, description, dateDue], (err) => {
-    if (err) console.error('Create operation error: ', err)
-    else console.log('Todo created successfully')
-  })
-})
+    if (err) console.error('Create operation error: ', err);
+    else {
+      const notification = new Notification({
+        title: 'Task Created',
+        body: `Task "${title}" created successfully.`
+      });
 
-// Read operation
+      notification.show();
+    }
+  });
+});
+
+// Read
 ipcMain.on('read-todos', (event) => {
-  const query = 'SELECT * FROM todos'
+  const query = 'SELECT * FROM todos';
   database.all(query, (err, rows) => {
-    if (err) console.error('Read operation error: ', err)
-    else event.reply('todos-read-reply', rows)
-  })
-})
+    if (err) console.error('Read operation error: ', err);
+    else event.reply('todos-read-reply', rows);
+  });
+});
 
-// Update operation
+// Update
 ipcMain.on('update-todo', (_, todo) => {
-  const { id, title, description, dateDue } = todo
+  const {
+    id,
+    title,
+    description,
+    dateDue,
+    notificationSentToday,
+    notificationSentTomorrow
+  } = todo;
 
   if (!id || !title || !dateDue) {
-    return
+    return;
   }
 
-  const query = 'UPDATE todos SET title=?, description=?, dateDue=? WHERE id=?'
-  database.run(query, [title, description, dateDue, id], (err) => {
-    if (err) console.error('Update operation error: ', err)
-    else console.log('Todo updated successfully')
-  })
-})
+  const query =
+    'UPDATE todos SET title=?, description=?, dateDue=?, notificationSentToday=?, notificationSentTomorrow=? WHERE id=?';
+  database.run(
+    query,
+    [
+      title,
+      description,
+      dateDue,
+      notificationSentToday,
+      notificationSentTomorrow,
+      id
+    ],
+    (err) => {
+      if (err) console.error('Update operation error: ', err);
+      else {
+        const notification = new Notification({
+          title: 'Task Updated',
+          body: `Task "${title}" updated successfully.`
+        });
 
-// Delete operation
+        notification.show();
+      }
+    }
+  );
+});
+
+// Delete
 ipcMain.on('delete-todo', (_, id) => {
-  if (!id) return
+  if (!id) return;
 
-  const query = 'DELETE FROM todos WHERE id=?'
+  const query = 'DELETE FROM todos WHERE id=?';
   database.run(query, [id], (err) => {
-    if (err) console.error('Delete operation error: ', err)
-    else console.log('Todo deleted successfully')
-  })
-})
+    if (err) console.error('Delete operation error: ', err);
+    else {
+      const notification = new Notification({
+        title: 'Task Deleted',
+        body: `Task deleted successfully.`
+      });
+
+      notification.show();
+    }
+  });
+});
+
+function checkDueDates() {
+  const now = new Date();
+  now.setHours(0, 0, 0, 0);
+
+  const tomorrow = new Date(now);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const dayAfter = new Date(tomorrow);
+  dayAfter.setDate(dayAfter.getDate() + 1);
+
+  const query = 'SELECT * FROM todos WHERE dateDue >= ? AND dateDue < ?';
+  database.all(
+    query,
+    [now.toISOString(), dayAfter.toISOString()],
+    (err, rows) => {
+      if (err) {
+        console.error('Error checking due tasks: ', err);
+        return;
+      }
+
+      console.log(rows);
+
+      rows.forEach((todo) => {
+        const isToday = tomorrow > new Date(todo.dateDue);
+        const isTomorrow = dayAfter > new Date(todo.dateDue);
+
+        if (isToday && !todo.notificationSentToday) {
+          const notificationToday = new Notification({
+            title: 'Task Due Today',
+            body: `Task "${todo.title}" is due today.`
+          });
+
+          notificationToday.show();
+
+          const updateQuery =
+            'UPDATE todos SET notificationSentToday=1 WHERE id=?';
+          database.run(updateQuery, [todo.id], (updateErr) => {
+            if (updateErr) {
+              console.error(
+                'Error updating notification status for today: ',
+                updateErr
+              );
+            }
+          });
+
+          return;
+        }
+
+        if (isTomorrow && !todo.notificationSentTomorrow) {
+          if (todo.notificationSentToday) return;
+
+          const notificationTomorrow = new Notification({
+            title: 'Task Due Tomorrow',
+            body: `Task "${todo.title}" is due tomorrow.`
+          });
+
+          notificationTomorrow.show();
+
+          const updateQuery =
+            'UPDATE todos SET notificationSentTomorrow=1 WHERE id=?';
+          database.run(updateQuery, [todo.id], (updateErr) => {
+            if (updateErr) {
+              console.error(
+                'Error updating notification status for tomorrow: ',
+                updateErr
+              );
+            }
+          });
+
+          return;
+        }
+      });
+    }
+  );
+}
--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,161 +1,181 @@
-import { useEffect, useRef, useState } from 'react'
-const { ipcRenderer } = window.electron
+import { useEffect, useRef, useState } from 'react';
+const { ipcRenderer } = window.electron;
+
+const displayDate = (date) => {
+  if (isNaN(date)) return '-';
+
+  const currentDate = new Date();
+  const tomorrowDate = new Date();
+
+  tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+
+  if (date.getDate() === currentDate.getDate()) {
+    return 'Today';
+  }
+
+  if (date.getDate() === tomorrowDate.getDate()) {
+    return 'Tomorrow';
+  }
+
+  return date.toDateString();
+};
 
 const App = () => {
-  const [todos, setTodos] = useState()
-  const [currentTodo, setCurrentTodo] = useState()
-  const titleElem = useRef()
-  const descriptionElem = useRef()
-  const dateElem = useRef()
+  const [todos, setTodos] = useState();
+  const [currentTodo, setCurrentTodo] = useState();
+  const titleElem = useRef();
+  const descriptionElem = useRef();
+  const dateElem = useRef();
 
   const createTodo = () => {
-    const title = titleElem.current.value
-    const description = descriptionElem.current.value
-    const dateDue = new Date(dateElem.current.value)
+    const title = titleElem.current.value;
+    const description = descriptionElem.current.value;
+    const dateDue = new Date(dateElem.current.value);
 
     if (!title || isNaN(dateDue)) {
-      return
+      return;
     }
 
-    const isoDate = dateDue.toISOString()
+    const isoDate = dateDue.toISOString();
 
     ipcRenderer.send('create-todo', {
       title: title,
       description: description,
       dateDue: isoDate
-    })
+    });
 
-    ipcRenderer.send('read-todos')
+    ipcRenderer.send('read-todos');
 
-    clear()
-  }
+    clear();
+  };
 
   const updateTodo = (id) => {
-    if (!id) return
+    if (!id) return;
 
-    const title = titleElem.current.value
-    const description = descriptionElem.current.value
-    const dateDue = new Date(dateElem.current.value)
+    const title = titleElem.current.value;
+    const description = descriptionElem.current.value;
+    const dateDue = new Date(dateElem.current.value);
 
     if (!title || isNaN(dateDue)) {
-      return
+      return;
     }
 
-    const isoDate = dateDue.toISOString()
+    const isoDate = dateDue.toISOString();
 
     ipcRenderer.send('update-todo', {
       id: id,
       title: title,
       description: description,
-      dateDue: isoDate
-    })
+      dateDue: isoDate,
+      notificationSent: false
+    });
 
-    ipcRenderer.send('read-todos')
+    ipcRenderer.send('read-todos');
 
-    clear()
-  }
+    clear();
+  };
 
   const deleteTodo = (id) => {
-    if (!id) return
+    if (!id) return;
 
-    ipcRenderer.send('delete-todo', id)
-    ipcRenderer.send('read-todos')
+    ipcRenderer.send('delete-todo', id);
+    ipcRenderer.send('read-todos');
 
-    setCurrentTodo()
-  }
+    setCurrentTodo();
+  };
 
   const updateInputs = (todo) => {
-    if (!todo) return
+    if (!todo) return;
 
-    titleElem.current.value = todo.title
-    descriptionElem.current.value = todo.description
-    dateElem.current.value = todo.dateDue.slice(0, 10)
+    titleElem.current.value = todo.title;
+    descriptionElem.current.value = todo.description;
+    dateElem.current.value = todo.dateDue.slice(0, 10);
 
-    setCurrentTodo(todo)
-  }
+    setCurrentTodo(todo);
+  };
 
   const clear = () => {
-    titleElem.current.value = ''
-    descriptionElem.current.value = ''
-    dateElem.current.value = ''
+    titleElem.current.value = '';
+    descriptionElem.current.value = '';
+    dateElem.current.value = '';
 
-    setCurrentTodo()
-  }
+    setCurrentTodo();
+  };
 
   useEffect(() => {
-    ipcRenderer.send('read-todos')
+    ipcRenderer.send('read-todos');
 
     ipcRenderer.on('todos-read-reply', (_, todos) => {
-      console.log(todos)
-      setTodos(todos)
-    })
+      console.log(todos);
+      setTodos(todos);
+    });
 
     return () => {
-      ipcRenderer.removeAllListeners('todos-read-reply')
-    }
-  }, [])
+      ipcRenderer.removeAllListeners('todos-read-reply');
+    };
+  }, []);
 
   return (
-    <div className="flex gap-6 w-full h-full">
-      <div className="flex flex-col gap-6 w-96 mt-0.5">
-        <h1 className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">
-          Selected: {currentTodo?.title || 'None'}
+    <div className='flex gap-6 w-full h-full'>
+      <div className='flex flex-col gap-6 w-96 mt-0.5 overflow-y-auto'>
+        <h1 className='block mb-2 text-2xl font-medium text-gray-900 dark:text-white'>
+          {currentTodo?.title || 'Add Task'}
         </h1>
         <div>
           <label
-            htmlFor="title"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor='title'
+            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
           >
             Title
           </label>
           <input
-            type="text"
-            name=""
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Write your title here..."
-            id="title"
+            type='text'
+            name=''
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            placeholder='Write your title here...'
+            id='title'
             ref={titleElem}
           />
         </div>
 
         <div>
           <label
-            htmlFor="description"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor='description'
+            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
           >
             Description
           </label>
           <textarea
-            id="description"
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Write your description here..."
+            id='description'
+            className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            placeholder='Write your description here...'
             ref={descriptionElem}
           ></textarea>
         </div>
 
         <div>
           <label
-            htmlFor="dateDue"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor='dateDue'
+            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
           >
             Date Due
           </label>
 
           <input
-            type="date"
-            name=""
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            id="dateDue"
+            type='date'
+            name=''
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            id='dateDue'
             ref={dateElem}
           />
         </div>
 
-        <div className="flex flex-col gap-2">
+        <div className='flex flex-col gap-2'>
           {currentTodo ? (
             <>
               <button
-                type="button"
-                className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+                type='button'
+                className='text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800'
                 disabled={!currentTodo}
                 onClick={() => updateTodo(currentTodo.id)}
               >
@@ -163,8 +183,8 @@ const App = () => {
               </button>
 
               <button
-                type="button"
-                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+                type='button'
+                className='text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800'
                 disabled={!currentTodo}
                 onClick={() => deleteTodo(currentTodo.id)}
               >
@@ -172,17 +192,17 @@ const App = () => {
               </button>
 
               <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                onClick={() => setCurrentTodo()}
+                type='button'
+                className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+                onClick={() => clear()}
               >
                 Cancel
               </button>
             </>
           ) : (
             <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              type='button'
+              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
               onClick={createTodo}
             >
               Add Task
@@ -191,79 +211,112 @@ const App = () => {
         </div>
       </div>
 
-      <div className="relative overflow-y-auto overflow-x-hidden shadow-md sm:rounded-lg w-full h-full">
-        <table className="w-full h-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className='relative overflow-y-auto overflow-x-hidden shadow-md sm:rounded-lg w-full h-full'>
+        <table className='w-full h-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+          <thead className='sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th
+                scope='col'
+                className='px-6 py-3'
+              >
                 ID
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th
+                scope='col'
+                className='px-6 py-3'
+              >
                 Title
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th
+                scope='col'
+                className='px-6 py-3'
+              >
                 Description
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th
+                scope='col'
+                className='px-6 py-3'
+              >
                 Created
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th
+                scope='col'
+                className='px-6 py-3'
+              >
                 Due
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th
+                scope='col'
+                className='px-6 py-3'
+              >
                 Tools
               </th>
             </tr>
           </thead>
           <tbody>
-            {todos &&
+            {todos && todos?.length ? (
               [...todos]
                 .sort((a, b) => new Date(a.dateDue) - new Date(b.dateDue))
                 .map((todo) => (
                   <tr
                     key={todo.id}
-                    className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                    className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'
                   >
-                    <th scope="row" className="px-6 py-4 max-w-40 overflow-hidden text-ellipsis">
+                    <th
+                      scope='row'
+                      className='px-6 py-4 max-w-40 overflow-hidden text-ellipsis'
+                    >
                       {todo.id}
                     </th>
-                    <td className="px-6 py-4 max-w-40 overflow-hidden text-ellipsis font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <td className='px-6 py-4 max-w-40 overflow-hidden text-ellipsis font-medium text-gray-900 whitespace-nowrap dark:text-white'>
                       {todo.title || '-'}
                     </td>
-                    <td className="px-6 py-4 max-w-40 overflow-hidden text-ellipsis">
+                    <td className='px-6 py-4 max-w-40 overflow-hidden text-ellipsis'>
                       {todo.description || '-'}
                     </td>
-                    <td className="px-6 py-4 max-w-40 overflow-hidden text-ellipsis">
-                      {todo.dateCreated ? new Date(todo.dateCreated).toDateString() : '-'}
+                    <td className='px-6 py-4 max-w-40 overflow-hidden text-ellipsis'>
+                      {todo.dateCreated
+                        ? new Date(todo.dateCreated).toDateString()
+                        : '-'}
                     </td>
-                    <td className="px-6 py-4 max-w-40 overflow-hidden text-ellipsis">
-                      {todo.dateDue ? new Date(todo.dateDue).toDateString() : '-'}
+                    <td className='px-6 py-4 max-w-40 overflow-hidden text-ellipsis'>
+                      {displayDate(new Date(todo.dateDue))}
                     </td>
-                    <td className="px-6 py-4">
+                    <td className='px-6 py-4'>
                       <a
-                        href="#"
-                        rel="noreferrer"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                        href='#'
+                        rel='noreferrer'
+                        className='font-medium text-blue-600 dark:text-blue-500 hover:underline'
                         onClick={() => updateInputs(todo)}
                       >
                         Edit
                       </a>
                       <a
-                        href="#"
-                        rel="noreferrer"
-                        className="ml-4 font-medium text-red-600 dark:text-red-500 hover:underline"
+                        href='#'
+                        rel='noreferrer'
+                        className='ml-4 font-medium text-red-600 dark:text-red-500 hover:underline'
                         onClick={() => deleteTodo(todo.id)}
                       >
                         Delete
                       </a>
                     </td>
                   </tr>
-                ))}
+                ))
+            ) : (
+              <tr>
+                <td
+                  className='text-center'
+                  colSpan='100%'
+                >
+                  No data available
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
